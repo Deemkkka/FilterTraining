2025df8293864d9f94b530046f5871dd70dfee0d
Deem <Deem@DEEM>
2022-06-14 10:10:40 +0300

    добавили массив данных

--- FilterTraining/MainWindow.xaml.cs
+++ FilterTraining/MainWindow.xaml.cs
@@ -20,9 +20,30 @@ namespace FilterTraining
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        User[] Users = new User[]
+            {
+                new User("Petr", "Russia", "1"),
+                new User("Petr2", "Russia", "2"),
+                new User("Petr3", "Russia", "1"),
+                new User("Petr4", "Russia", "3"),
+                new User("Petr5", "Russia", "1"),
+                new User("Petr6", "Russia", "2"),
+                new User("Petr7", "Russia", "1"),
+                new User("Petr8", "Russia", "1"),
+                new User("Petr9", "Russia", "5"),
+                new User("Petr10", "Russia", "1"),
+                new User("Petr11", "Russia", "4"),
+                new User("Petr12", "Russia", "1")
+            };
+
         public MainWindow()
         {
             InitializeComponent();
+
+            myListView.ItemsSource = Users;
+
+            FilterComboBox.ItemsSource = new string[] { "Name", "Country", "Status" };
         }
     }
 }
